e125664ea1f2e110a9a8def15f0de28aa25a6afd
abhitpahwa <abhitpahwa99@gmail.com>
2018-09-01 21:36:38 +0530

    Update dv_lab1_a.R

--- dv_lab1_a.R
+++ dv_lab1_a.R
@@ -2,6 +2,8 @@
 
 library(ggplot2)
 dataset<-read.csv("dataset_lab1.csv",sep=",",header=TRUE)
+
+#bar plot and line plot
 gg<-ggplot(dataset,aes(x=Age.group,y=Number.of.loans,group=1))+
   geom_bar(stat="identity")+
   geom_line(aes(y=Bad.rate*2000),col="red",size=1.5)+
